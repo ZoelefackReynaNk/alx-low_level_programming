6f9f690d0b6eb5b5f9892170068c181d70db39c9
Zoelefack Reyna Nkoumbouk <zoelefackreyna246@gmail.com>
2022-09-16 18:17:48 +0100

    sum of multiples

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * print sum of  all multiples of 3 
+ * or 5 below 1024,
+ * followed by a new line
+ */
+int main()
+{
+        int sum = 0;
+        
+        for (int i = 0; i <= 1024; i++){
+                if (i%3 == 0 || i %5 == 0){
+                     sum = sum + i;   
+                }
+                else(){
+                        continue
+                }
+        }
+        printf("%d\n", sum);
+        return (0);
+}
